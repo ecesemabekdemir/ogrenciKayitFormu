69f289af8c49aada4e8dee77213ce329b0de2671
Doğa Savaş <doasavaas@gmail.com>
2024-09-29 13:50:27 +0300

    hata düzeltildi

--- action/basvuru.js
+++ action/basvuru.js
@@ -1,15 +1,16 @@
 "use server";
 
 export async function BasvuruYapAksiyonu(prevState, values) {
-  let errors = {};
-  if (formObj.step == 1) {
+  let errors = {}
+  
+  if(values.step == 1){
     errors = {
-      name: !values.name && "İsim boş olamaz",
-      surname: !values.surname && "Soyad boş olamaz",
-      phone: !formObj.phone && "Telefon alanı boş olamaz",
-      tcno: !formObj.tcno && "TCNO alanı boş olamaz",
+        name: !values.name && "İsim boş olamaz",
+        surname: !values.surname && "Soyad boş olamaz",
+        phone: !values.phone && "Telefon alanı boş olamaz",
+        tcno: !values.tcno && "TCNO alanı boş olamaz"
     };
-  }
+}
 
   return { errors };
 }
