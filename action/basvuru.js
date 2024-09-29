872500a3a6f275a5b451c52629b90800005a4934
Sema Bekdemir <ecesemadimen96@gmail.com>
2024-09-29 13:53:11 +0300

    degisiklikler kaydedildi

--- action/basvuru.js
+++ action/basvuru.js
@@ -1,13 +1,13 @@
 "use server";
 
-export async function BasvuruYapAksiyonu(prevState, values) {
+export async function BasvuruYapAksiyonu(values) {
   let errors = {};
-  if (formObj.step == 1) {
+  if (values.step == 1) {
     errors = {
       name: !values.name && "İsim boş olamaz",
       surname: !values.surname && "Soyad boş olamaz",
-      phone: !formObj.phone && "Telefon alanı boş olamaz",
-      tcno: !formObj.tcno && "TCNO alanı boş olamaz",
+      phone: !values.phone && "Telefon alanı boş olamaz",
+      tcno: !values.tcno && "TCNO alanı boş olamaz",
     };
   }
 
